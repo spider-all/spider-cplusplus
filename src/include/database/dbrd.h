bf0c40a888cf074a3963a36916408834c2d309a6
Tosone <i@tosone.cn>
2020-09-24 20:23:20 +0800

    :construction: Change inc to include

--- src/include/database/dbrd.h
+++ src/include/database/dbrd.h
@@ -11,14 +11,14 @@
 
 class DBRD : public Database {
 private:
-  int set_value(std::string, std::string);
-  int get_value(std::string, std::string *);
+  int set_value(const std::string&, const std::string&);
+  int get_value(const std::string&, std::string *);
   std::mutex mtx;
 
 public:
   DBRD(const std::string&, int);
-  ~DBRD();
-  int initialize();
-  int create_user(user);
-  std::vector<std::string> list_users();
+  ~DBRD() override;
+  int initialize() override;
+  int create_user(user) override;
+  std::vector<std::string> list_users() override;
 };
