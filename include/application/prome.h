9510c0e0f6e9a5566188aab7af01ede0c4bfe4b1
Tosone <i@tosone.cn>
2021-09-12 21:15:24 +0800

    :bookmark: Add prometheus

---
+++ include/application/prome.h
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <prometheus/counter.h>
+#include <prometheus/exposer.h>
+#include <prometheus/registry.h>
+
+#include <config.h>
+
+#include <application/application.h>
+#include <database/database.h>
+
+#pragma once
+
+class Prome : public Application {
+private:
+  Config config;
+  Database *database;
+
+  int semaphore = 0; // 执行过程中的信号量
+  bool stopping = false;
+
+  prometheus::Exposer *exposer;
+
+public:
+  Prome(Config, Database *);
+  ~Prome() override;
+  int startup() override;
+};
