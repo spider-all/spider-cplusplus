efddc45fbe8048e48ae054e2fb533e99f92a9db6
Tosone <i@tosone.cn>
2021-09-21 19:53:23 +0800

    :fire: Remove useless code

--- include/const.h
+++ include/const.h
@@ -2,8 +2,4 @@
 
 #pragma once
 
-const std::string DATABASE_MYSQL = "mysql";
-const std::string DATABASE_SQLTE3 = "sqlite3";
 const std::string DATABASE_MONGODB = "mongodb";
-const std::string DATABASE_LEVELDB = "leveldb";
-const std::string DATABASE_POSTGRESQL = "postgresql";
--- src/config.cc
+++ src/config.cc
@@ -9,20 +9,8 @@ int Config::initialize(const std::string &config_path) {
   crawler_sleep_each_request = config["sleep"].as<int64_t>();
 
   database_type = config["database"]["type"].as<std::string>();
-  if (database_type == DATABASE_SQLTE3) {
-    database_sqlite3_path = config["database"][DATABASE_SQLTE3]["path"].as<std::string>();
-  } else if (database_type == DATABASE_LEVELDB) {
-    database_leveldb_path = config["database"][DATABASE_LEVELDB]["path"].as<std::string>();
-  } else if (database_type == DATABASE_MONGODB) {
+  if (database_type == DATABASE_MONGODB) {
     database_mongodb_dsn = config["database"][DATABASE_MONGODB]["dsn"].as<std::string>();
-  } else if (database_type == DATABASE_MYSQL) {
-    database_mysql_host = config["database"][DATABASE_MYSQL]["host"].as<std::string>();
-    database_mysql_user = config["database"][DATABASE_MYSQL]["user"].as<std::string>();
-    database_mysql_password = config["database"][DATABASE_MYSQL]["password"].as<std::string>();
-    database_mysql_db = config["database"][DATABASE_MYSQL]["db"].as<std::string>();
-    database_mysql_port = config["database"][DATABASE_MYSQL]["port"].as<unsigned int>();
-  } else if (database_type == DATABASE_POSTGRESQL) {
-    database_mongodb_dsn = config["database"][DATABASE_POSTGRESQL]["dsn"].as<std::string>();
   }
 
   this->crawler_type_followers = config["crawler_type"]["followers"].as<bool>();
